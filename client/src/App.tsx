05321d54cefe297d4dee1af773815eb4afddb9b1
Vrishank Sharma <sharma08462@gmail.com>
2024-07-03 15:26:22 +0530

    Project initialized

---
+++ client/src/App.tsx
@@ -0,0 +1,17 @@
+import Header from "./ui/Header";
+
+
+function App() {
+  
+
+  return (
+    <main>
+      <Header/>
+      <p>
+        Ecommerce
+      </p>
+    </main>
+  );
+}
+
+export default App
